3f8c74e9e03f077b6810b4a51f4473ee9b12ffe2
Athul CS <athulcs.cs@gmail.com>
2018-07-25 15:12:41 +0530

    Devs Fragment added

--- app/src/main/java/com/texopanda/texopandaapp/ScrollingActivity.java
+++ app/src/main/java/com/texopanda/texopandaapp/ScrollingActivity.java
@@ -1,6 +1,9 @@
 package com.texopanda.texopandaapp;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -11,9 +14,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class ScrollingActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ScrollingActivity extends AppCompatActivity
+                            implements DevsFragment.OnFragmentInteractionListener{
+    private CoordinatorLayout coordinatorLayout;
     private DrawerLayout mDrawerLayout;
+    private FirebaseAuth auth;
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +41,11 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
         mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        auth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference("users").child(auth.getUid());
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
@@ -37,13 +61,47 @@ public class ScrollingActivity extends AppCompatActivity {
                         menuItem.setChecked(true);
                         // close drawer when item is tapped
                         mDrawerLayout.closeDrawers();
-
+                        switch (menuItem.getItemId()){
+                            case R.id.nav_logout:logout();break;
+                            case R.id.nav_reg_events:;break;
+                            case R.id.nav_support:;break;
+                            case R.id.nav_dev:
+                                DevsFragment devsFragment = new DevsFragment();
+                                getSupportFragmentManager().beginTransaction()
+                                .add(R.id.coordinator_layout,devsFragment)
+                                .commit();break;
+                        }
                         // Add code here to update the UI based on the item selected
                         // For example, swap UI fragments here
 
                         return true;
                     }
                 });
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                List<Boolean> eventsCard = new ArrayList<Boolean>(Arrays.asList(new Boolean[4]));
+                eventsCard=user.getEvents();
+                for(int i=0;i<eventsCard.size();i++){
+                    if(eventsCard.get(i)){
+                        Button button;
+                        int resourceId = ScrollingActivity.this.getResources().
+                                getIdentifier("button"+i, "id", ScrollingActivity.this.getPackageName());
+                        button = findViewById(resourceId);
+                        button.setText("REGISTERED");
+                        }
+                    }
+
+                }
+
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+
+        });
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -68,4 +126,18 @@ public class ScrollingActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void logout(){
+        auth.signOut();
+        finish();
+    }
+    public void register(View view){
+        dbRef.child("events").child(view.getTag().toString()).setValue(true);
+        Button button = (Button) view;
+        button.setText("REGISTERED");
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
